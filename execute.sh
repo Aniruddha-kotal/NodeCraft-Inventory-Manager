e7a2bd93b58f368e73115d4593c3c4bbab574870
Aniruddha-Kotal <aniruddhakotal0203@gmail.com>
2023-12-01 18:00:37 +0530

    same

--- execute.sh
+++ execute.sh
@@ -4,4 +4,4 @@ echo "$EC2_SSH_KEY_CONTENT" > ec2-private-key.pem
 chmod 600 ec2-private-key.pem
 
 rsync -avz --exclude 'node_modules' -e "ssh -i ec2-private-key.pem" ./ ec2-user@3.145.3.17:/home/ec2-user/NodeProject/node
-ssh -i ec2-private-key.pem ec2-user@your-ec2-instance-ip 'cd /home/ec2-user/NodeProject/node && npm install && pm2 restart server.js'
+ssh -i ec2-private-key.pem ec2-user@3.145.3.17 'cd /home/ec2-user/NodeProject/node && npm install && pm2 restart server.js'
